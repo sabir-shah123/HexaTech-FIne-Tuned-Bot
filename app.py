97edfb2747afbfe902af56cfd514d8fcc58c7e44
sabir-shah123 <sabirshahbzu@gmail.com>
2023-07-14 16:06:41 +0500

    d

--- app.py
+++ app.py
@@ -90,5 +90,4 @@ if __name__ == '__main__':
         print("Training folder does not exist. Please create a folder named " + training_folder + " and add training files to it.")
         exit()
     index = construct_index(training_folder)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
